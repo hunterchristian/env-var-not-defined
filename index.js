b8437ca551a0e98dcf815f023545e4aa06691714
Hunter Hodnett <hchodnett@gmail.com>
2020-02-01 11:38:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-class ElementNotFound extends Error {
+class EnvVarNotDefined extends Error {
   constructor(envVar) {
     super(`environment variable must be defined: ${ envVar }`);
-    Error.captureStackTrace(this, ElementNotFound);
+    Error.captureStackTrace(this, EnvVarNotDefined);
   }
 }
 
 module.exports = (envVar) => {
   const MAYBE_ENV_VAR = process.env[envVar];
   if (!MAYBE_ENV_VAR) {
-    throw new ElementNotFound(envVar);
+    throw new EnvVarNotDefined(envVar);
   }
 
   return MAYBE_ENV_VAR;
